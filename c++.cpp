f4603150c3b39854a9cd4cef32bd4bbe674bd2b7
yetto-tools <ergonzalez209@gmail.com>
2020-11-22 17:13:03 -0600

    second upload files

---
+++ c++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(){
+	
+
+	for(int a= 0; a<10; a++){
+    
+		cout<<"Hola mundo en nuestro compilador...."<<endl;
+   
+	}
+    
+	cin.get();
+    
+	return 0;
+
+}	
+
